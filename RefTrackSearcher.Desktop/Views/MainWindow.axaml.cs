b1316a26dd11d3b9c1f9bd1314492418c84e9c71
Dzmitry <dzmi3y@gmail.com>
2025-11-09 21:45:57 +0300

    Add project files.

---
+++ RefTrackSearcher.Desktop/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace RefTrackSearcher.Desktop.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RefTrackSearcher.ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace RefTrackSearcher.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
